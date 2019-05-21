379638b27b14dc8fdd8272f4c9fbf44e2154094e
seo218 <casey.seo1@gmail.com>
2019-05-21 10:20:11 -0400

    'routes_update'

--- server/controllers.js
+++ server/controllers.js
@@ -1,5 +1,8 @@
 const express = require('express')
-const { getRestaurantInfoModel } = require('./models.js')
+const { getRestaurantInfoModel,
+        getPicturePopUpInfoModel,
+        getRatingsInfoModel
+      } = require('./models.js')
 
 module.exports = {
   getRestaurantInfoController: (req, res) => {
@@ -7,14 +10,44 @@ module.exports = {
     getRestaurantInfoModel(restaurantId, (err, data) => {
       if(err) {
         res.status(500)
-        console.log('failed to retrieve random restaurant from db at contoller', err)
+        console.log('failed to retrieve random restaurant at contoller', err)
         res.send()
       } else {
         res.status(200)
-        console.log('got random restaurant from db at contoller', data)
+        console.log('got random restaurant from db at contoller')
         res.send(data)
       }
     })
   },
+
+  getPicturePopUpInfoController: (req, res) => {
+    let restaurantId = req.query.restaurant
+    getPicturePopUpInfoModel(restaurantId, (err, data) => {
+      if(err) {
+        res.status(500)
+        console.log('falied to get picturePopUp info at controller =>', err)
+        res.send()
+      } else {
+        res.status(200)
+        console.log('got picture pop up info at contorller')
+        res.send(data)
+      }
+    })
+  },
+
+  getRatingsInfoController: (req, res) => {
+    let restaurantId = req.query.restaurant
+    getRatingsInfoModel(restaurantId, (err, data) => {
+      if(err) {
+        res.status(500)
+        console.log('failed to get ratings info at controller =>', err)
+        res.send()
+      } else {
+        res.status(200)
+        console.log('got ratings info at controller')
+        res.send(data)
+      }
+    })
+  }
 
 }
--- server/models.js
+++ server/models.js
@@ -1,53 +1,44 @@
-const {db}  = require('../db/index.js')
+const {db}  = require('../db/index.js');
 
-
-console.log('expect db', db)
 
 getRestaurantInfoModel = (restaurant, cb) => {
-  let queryStr = db.prepare(`SELECT * FROM restaurants
-  WHERE id = ${restaurant};`)
-  db.serialize(() => {
-    queryStr.run( (err, data) => {
-      if(err) console.log('err in getting restaurant from db at model', err)
-      else console.log('got restaurant from db at model', data)
-    })
-  })
-  // db.serialize(() => {
-  //   console.log('expect open db', db)
-  //   let restaurantsQuery = db.prepare(
-  //     `SELECT * FROM restaurants
-  //     WHERE id = ${restaurant};`
-  //   )
-  //   restaurantsQuery.run(restaurant, (err, data) => {
-  //     cb(err, data)
-  //   })
-  // })
-  
-  // picturePopUpQueryFunc = () => {
-  //   let picturePopUpQuery = 
-  //   `SELECT * from picturePopUp
-  //   WHERE id = ${restaurantNumber};`
-  //   db.all(picturePopUpQuery, (err, data) => {
-  //     cb(err, data)
-  //   })
-  // } 
-  // ratingsQueryFunc = () => {
-  //   let ratingsQuery = 
-  //   `SELECT * FROM ratings
-  //   WHERE id = ${restaurantNumber};`
-  //   db.all(ratingsQuery, (err, data) => {
-  //     cb(err, data)
-  //   })
-  // }
-  // restaurantQueryFunc()
-  // picturePopUpQueryFunc()
-  // ratingsQueryFunc()
-  },
-  
-    
+	db.all(
+		`SELECT * FROM restaurants
+    WHERE id = 50;`,
+		(err, res) => {
+		  if (err) console.log('failed to get restaurtant info from db at model =>', err);
+		  else cb(null, res);
+		}
+	);
+}
+
+getPicturePopUpInfoModel = (restaurant, cb) => {
+  db.all(
+    `SELECT * FROM picturePopUp
+    WHERE restaurantId = 50`,
+    (err, res) => {
+      if(err) console.log('falied to get picturePopUp info from db at model =>', err)
+      else cb(null, res)
+    }
+  )
+}
+
+getRatingsInfoModel = (restaurant, cb) => {
+  db.all(
+    `SELECT * FROM ratings
+    WHERE restaurantID = 50`,
+    (err, res) => {
+      if (err) console.log('failed to get ratings info form db at model', err)
+      else cb(null, res)
+    }
+  )
+}
+
 
 module.exports = {
-  getRestaurantInfoModel
+  getRestaurantInfoModel,
+  getPicturePopUpInfoModel,
+  getRatingsInfoModel
 }
 
 
