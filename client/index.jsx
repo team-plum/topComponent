83f91877c87bb71365af3881830905baf3de64f1
seo218 <casey.seo1@gmail.com>
2019-05-23 16:17:56 -0400

    all components getting db data

--- client/index.jsx
+++ client/index.jsx
@@ -1,23 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
 import NameReviewsDollarSignsAndCuisines from './components/NameReviewsDollarSignsAndCuisines.jsx'
+import Map from './components/Map.jsx'
+import ImageCarousel from './components/ImageCarousel.jsx'
 
 class App extends React.Component{
   constructor(props){
     super(props)
-    // this.state = {
-
-    // }
-    //bind
+    this.state = {
+      restaurant: this.randomRestaurant(),
+      restaurantInfo: null
+    }
+    this.randomRestaurant = this.randomRestaurant.bind(this)
+    this.getRestaurantInfo = this.getRestaurantInfo.bind(this)
   }
 
+  componentDidMount() {
+    
+  }
+
+  randomRestaurant () {
+    return Math.ceil(Math.random() * 100)
+  }
+
+  getRestaurantInfo (restaurant) {
+    axios.get('/restaurant', {params: {restaurant: restaurant}})
+    .then((data) => {
+      this.setState({
+        restaurantInfo: data.data[0]
+      })
+      // console.log('expect restaurant info => ', data.data[0])
+    })
+    .catch((err) => {
+      console.log('failed to get restaurant info at client', err)
+    })
+  }
 
   render() {
     return (
       <div className="app">
         <div className="restaurantName">
-          < NameReviewsDollarSignsAndCuisines />
+          <NameReviewsDollarSignsAndCuisines 
+            restaurant = {this.state.restaurant}
+            getRestaurantInfo = {this.getRestaurantInfo}
+            restaurantInfo = {this.state.restaurantInfo}
+          />
           
+        </div>
+        <div className="map">
+          <Map 
+            restaurantInfo = {this.state.restaurantInfo}
+          />
+        </div>
+        <div className="imageCarousel">
+          <ImageCarousel
+            restaurant = {this.state.restaurant}
+          />
         </div>
         
       </div>
